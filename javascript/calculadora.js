e379217247dd3b8fa8b6d412857ac30130cb14ee
brunovictor21 <brunovictor21@gmail.com>
2025-09-04 16:17:57 -0300

    calculadora

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -1,4 +1,41 @@
-// Variaveis 
-    const num1 = getElementById("Numero 1");
-    const num1 = getElementById("Numero 2");
-    
+// Variável
+const num1 = document.getElementById("numero1");
+const num2 = document.getElementById("numero2");
+const Resultado = document.getElementById("resultado");
+
+// Função
+function somar(event){
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) + Number(num2.value);
+    num1.value ="";
+    num2.value ="";    
+}
+
+function sub(event){
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) - Number(num2.value);
+    num1.value ="";
+    num2.value ="";    
+}
+
+function mult(event){
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) * Number(num2.value);
+    num1.value ="";
+    num2.value ="";    
+}
+
+function div(event){
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) / Number(num2.value);
+    num1.value ="";
+    num2.value ="";    
+}
