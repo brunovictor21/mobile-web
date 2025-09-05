da0a46dce741c9729b9227241ecb638bfe8ceed5
brunovictor21 <brunovictor21@gmail.com>
2025-09-05 14:50:13 -0300

    aula de IF

---
+++ javascript/js-exemplos/idade.js
@@ -0,0 +1,13 @@
+/* 
+Se idade >= 18, pessoa pode ir para festa
+senão, a pessoa não pode ir para festa.
+
+*/ 
+let idade = 18; 
+if (idade >=18) { 
+        console.log("Pode ir para a festa");
+}
+else {
+     console.log("Pode não pode ir para a festa");
+}
+   
---
+++ javascript/js-exemplos/nota.js
@@ -0,0 +1,13 @@
+/*
+Se a nota for >= 6, o aluno está aprovado
+Senão ele está reprovado.
+*/
+
+let nota = 7;
+
+if (nota >= 6) { 
+        console.log("Aprovado");
+}
+else {
+     console.log("Reprovado");
+}
