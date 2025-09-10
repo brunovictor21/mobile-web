c0c054e2f7048f7cd651f19eb65b637b21d7ac7a
brunovictor21 <brunovictor21@gmail.com>
2025-09-10 15:00:07 -0300

    not

--- javascript/nota.js
+++ javascript/nota.js
@@ -1,1 +1,8 @@
-alert("Olá Rodrigo");
+// Variaveis
+const inputNota = document.getElementById("nota");
+const inputNota = document.getElementById("result");
+
+// Função
+function resultadoNotaClick() {
+    alert("Resultado");
+}
