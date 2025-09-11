a528538aa2f1decfb7cfc5c9fad4d30728deb0fe
brunovictor21 <brunovictor21@gmail.com>
2025-09-11 14:41:13 -0300

    nota message

--- javascript/nota.js
+++ javascript/nota.js
@@ -1,10 +1,13 @@
 // Variaveis
 const inputNota = document.getElementById("nota");
 const result = document.getElementById("result");
+const message = document.getElementById("message");
 
 // Função
 function resultadoNotaClick(event) {
     event.preventDefault();
+
+ 
 
     // logica - condição 
     if (inputNota.value >= 6) {
@@ -12,6 +15,14 @@ function resultadoNotaClick(event) {
     } else {
         result.innerHTML = "REPROVADO";
     }
+
+    //Validação de formulário
+ if (inputNota.value == ""); 
+    message.style.display = "block";
+        return false;
+
+        message.style.display = "none";
+
 }
 
 
