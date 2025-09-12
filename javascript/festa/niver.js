1bb1a64142fc390f7c3ed5c52d9730fbea8f5ff3
brunovictor21 <brunovictor21@gmail.com>
2025-09-12 16:20:55 -0300

    festa

--- javascript/festa/niver.js
+++ javascript/festa/niver.js
@@ -1,14 +1,14 @@
 function diasParaNiver() {
     const hoje = new Date();
-    const dia semana = hoje.getDay(); // 0=Dom, 1=Seg, ...,4=Quin
+    const diaSemana = hoje.getDay(); // 0=Dom, 1=Seg, ...,4=Quin
     let diasFaltando = 4 - diaSemana;
 
-    if(diasFaltando < 0){
+    if (diasFaltando < 0){
         diasFaltando += 7;
     }
 
-    document.getElementById("dias").textContent = diasFaltando
-    
+    document.getElementById("dias").textContent = diasFaltando; 
+
 }
 
 
